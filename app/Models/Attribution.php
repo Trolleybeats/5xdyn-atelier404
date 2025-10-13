fd1d504991f672e4c3c7bfd8ff238d8ea0f39f4f
louvr197 <134929961+louvr197@users.noreply.github.com>
2025-10-13 16:05:53 +0200

    Add role column to users table with default value

--- app/Models/Attribution.php
+++ app/Models/Attribution.php
@@ -9,4 +9,19 @@ class Attribution extends Model
 {
     /** @use HasFactory<\Database\Factories\AttributionFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'intervention_id',
+        'user_id',
+    ];
+
+    public function intervention()
+    {
+        return $this->belongsTo(Intervention::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- database/migrations/2025_10_13_134409_create_images_table.php
+++ database/migrations/2025_10_13_134409_create_images_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('path');
+            //foreign
+            $table->foreignId('note_id')->constrained('notes')->onDelete('cascade');
         });
     }
 
