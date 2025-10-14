3b81c423b0e2dab9634e2c6da430da2ffe84e13f
louvr197 <134929961+louvr197@users.noreply.github.com>
2025-10-14 15:40:00 +0200

    Add custom Blade components and update welcome page
    Introduces new Blade components for FAQ, intervention form, localisation, and device type selection, along with their PHP classes. Updates TypeAppareil model with fillable attributes, relationships, and a static getter. Refactors the welcome page to use these new components and removes the default Laravel content.

--- app/Models/TypeAppareil.php
+++ app/Models/TypeAppareil.php
@@ -9,4 +9,19 @@ class TypeAppareil extends Model
 {
     /** @use HasFactory<\Database\Factories\TypeAppareilFactory> */
     use HasFactory;
+<<<<<<< Updated upstream
+=======
+    protected $fillable = [
+        'nom',
+    ];
+    public function interventions()
+    {
+        return $this->hasMany(Intervention::class);
+    }
+
+    public static function getAll()
+    {
+        return self::all();
+    }
+>>>>>>> Stashed changes
 }
---
+++ resources/views/components/formulaire-intervention.blade.php
@@ -0,0 +1,44 @@
+@php
+    use App\Models\TypeAppareil;
+@endphp
+<div>
+    <form method="POST" action="{{-- route('client.create') --}}">
+        @csrf
+        <fieldset>
+            {{-- Nom --}}
+            <div>
+                <x-input-label for="nom" :value="__('Nom')" />
+                <x-text-input id="nom" class="block mt-1 w-full" type="text" name="nom" :value="old('nom')"
+                    required />
+                <x-input-error :messages="$errors->get('nom')" class="mt-2" />
+            </div>
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+            {{-- Telephone --}}
+            <div>
+                <x-input-label for="telephone" :value="__('telephone')" />
+                <x-text-input id="telephone" class="block mt-1 w-full" type="tel" name="telephone" :value="old('telephone')"
+                    required />
+                <x-input-error :messages="$errors->get('tel')" class="mt-2" />
+            </div>
+        </fieldset>
+        <fieldset>
+            {{-- type Appareil --}}
+            <div>
+
+                <x-input-label for="typeAppareil" :value="__('typeAppareil')" />
+                <x-selection-type-appareil id="typeAppareil" name="typeAppareil" :collection=" TypeAppareil::getAll() " class="block mt-1 w-full" ></x-selection-type-appareil>
+                <x-input-error :messages="$errors->get('typeAppareil')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="description" :value="__('description')"/>
+                <textarea name="description" id="description" cols="auto" rows="10"></textarea>
+            </div>
+        </fieldset>
+    </form>
+</div>
