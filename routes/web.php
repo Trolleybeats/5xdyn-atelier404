07a2c42fb68e2caee1b074b4de2c0b14227bc392
Trolleybeats <nathanyouhout450@gmail.com>
2025-10-17 14:11:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
-use App\Models\Intervention;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
