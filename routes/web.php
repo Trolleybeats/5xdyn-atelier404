aea6590142924ace50956b805172289043374a3b
louvr197 <134929961+louvr197@users.noreply.github.com>
2025-10-17 15:47:11 +0200

    Add intervention and client management features
    Introduced ClientController and InterventionController for handling client and intervention creation. Added InterventionPolicy for authorization. Updated TypeAppareilFactory and TypeAppareilSeeder to generate type appareils. Added a localisation Blade component. Modified guest layout to link logo to login. Expanded web routes to support intervention creation and user management. Resolved merge conflicts in DatabaseSeeder and routes/web.php.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
+<<<<<<< Updated upstream
+=======
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InterventionController;
+>>>>>>> Stashed changes
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+<<<<<<< Updated upstream
 });
+=======
+})->name('welcome');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::get('/interventions/create', [InterventionController::class, 'create'])->name('interventions.create');
+Route::post('/interventions', [InterventionController::class, 'store'])->name('interventions.store');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+//Routes pour la gestion des utilisateurs
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+
+    // Gestion des utilisateurs (Détails et changement de rôle)
+    Route::resource('/users', UserController::class);
+});
+
+require __DIR__ . '/auth.php';
+>>>>>>> Stashed changes
