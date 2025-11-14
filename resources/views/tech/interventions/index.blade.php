88f67d4bbe699e22a5119da96fe436d704346de7
Trolleybeats <nathanyouhout450@gmail.com>
2025-11-14 15:54:18 +0100

    Redesign dashboard and navigation UI
    Refactors dashboard and navigation layouts for a sidebar-based design, updates dropdown and header components for consistent user profile access, and improves intervention filtering to allow selection of unassigned interventions. Also adjusts container widths and grid layouts for better responsiveness and usability.

--- resources/views/tech/interventions/index.blade.php
+++ resources/views/tech/interventions/index.blade.php
@@ -1,12 +1,38 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Interventions') }}
-        </h2>
+     <x-slot name="header">
+        <div class="flex items-center justify-end">
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button
+                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-700 bg-white focus:outline-none transition">
+                        <span class="px-3 py-1 text-grey-700 rounded-md">{{ Auth::user()->name }}</span>
+                        <svg class="ms-2 fill-current h-4 w-4" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <x-dropdown-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
+                    </x-dropdown-link>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault(); this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
+                </x-slot>
+            </x-dropdown>
+        </div>
     </x-slot>
 
     <div class="py-6 sm:py-12">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 md:ml-64">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-4 sm:p-6 lg:px-20 bg-white border-b border-gray-200">
                     <!-- Header responsive -->
